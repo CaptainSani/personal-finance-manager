bc67758b3d00357e68f35096eb75b1a9bea468df
omotomiwa26 <omotomiwa26@gmail.com>
2024-12-19 07:36:35 +0100

    REFACTOR:Add CORS support and dotenv configuration to app.js

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require("express");
 const app = express();
 const authRoutes = require("./routes/authRoutes");
 const budgetRoutes = require("./routes/budgetRoutes");
+const cors = require('cors');
+const dotenv = require('dotenv');
 
 const port = 3000;
 
+dotenv.config();
+
 app.use(express.json());
+app.use(cors());
 app.use("/api/auth", authRoutes);
 
 
