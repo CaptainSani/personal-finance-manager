15affb095f3ea4b61a850dcb34365fb92f4b6aff
omotomiwa26 <omotomiwa26@gmail.com>
2024-12-19 08:44:58 +0100

    REFACTOR: Update success message format in user registration response

--- controller/authController.js
+++ controller/authController.js
@@ -13,10 +13,17 @@ const authController = {
   
         const user = await User.createUser(username, email, password);
         const createdUser = user;
+<<<<<<< HEAD
         // const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
         //   expiresIn: '1h'
         // });
         res.status(200).json({ user: createdUser, text: 'User created succesfully' });
+=======
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+          expiresIn: process.env.JWT_SECRET_EXPIRY,
+        });
+        res.status(200).json({message: `User ${username} Registered succesfully` });
+>>>>>>> 991a07f (REFACTOR: Update success message format in user registration response)
       } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Registration failed, Login ?' });
