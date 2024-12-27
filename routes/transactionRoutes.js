3c7719dea98933a9d43da41c6e3c74cc614c13d8
omotomiwa26 <omotomiwa26@gmail.com>
2024-12-27 20:06:30 +0100

    FIX: Update transaction route to use a more descriptive path for creating transactions

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const transactionController = require('../controller/transactionController');
 const { authenticate } = require('../midddleware/auth');
 
-router.post('/transactions', authenticate, transactionController.createTransaction);
+router.post('/transactions/create', authenticate, transactionController.createTransaction);
 router.get('/transactions', authenticate, transactionController.getAllTransactions);
 router.get('/transactions/:id', authenticate, transactionController.getTransactionById);
 router.put('/transactions/:id', authenticate, transactionController.updateTransaction);
