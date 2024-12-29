752b476cf217665a760b12f8a7173e76786e0843
omotomiwa26 <omotomiwa26@gmail.com>
2024-12-29 16:09:53 +0100

    FIX: Correct middleware import path and update user route paths for consistency

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controller/authController');
 const userController = require('../controller/userController');
-const { authenticate } = require('../midddleware/auth');
+const { authenticate } = require('../middleware/auth');
 
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/user/:email', authenticate, userController.getUserByEmail);
 router.get('/user/id/:id', authenticate, userController.getUserById);
-router.patch('/users/username/:id', authenticate, userController.updateUsername);
-router.patch('/users/email/:id', authenticate, userController.updateEmail);
-router.patch('/users/password/:id', authenticate, userController.updatePassword);
+router.patch('/user/username/:id', authenticate, userController.updateUsername);
+router.patch('/user/email/:id', authenticate, userController.updateEmail);
+router.patch('/user/password/:id', authenticate, userController.updatePassword);
 
 module.exports = router;
