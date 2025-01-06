df8c1301975fc807f0f5af2e8da55a4b8de96972
omotomiwa26 <omotomiwa26@gmail.com>
2025-01-06 13:18:04 +0100

    REFACTOR: remove updateUserDetails method for improved code simplicity

--- controller/userController.js
+++ controller/userController.js
@@ -84,47 +84,6 @@ const userController = {
     }
   },
 
-  async updateUserDetails(req, res) {
-    try {
-      const id = req.params.id;
-      const { username, password } = req.body;
-
-      if (!id || !username || !password) {
-        return res.status(400).json({
-          status: "Bad Request",
-          statusCode: 400,
-          error: "Please Input All Required Fields",
-        });
-      }
-      const updatedUser = await User.updateUserDetails(id, username, password);
-
-      if (!updatedUser) {
-        res.status(404).json({
-          status: "Not Found",
-          statusCode: 404,
-          message: `User not found`,
-        });
-      }
-
-      return res.status(200).json({
-          status: "Success OK",
-          statusCode: 200,
-          message: `User Updated Succesfully`,
-          user: {
-            id: updatedUser.id,
-            username: updatedUser.username,
-          },
-        });
-      
-    } catch (err) {
-      console.error("Error in updateUser:", err);
-      res.status(500).json({
-        status: "Internal Server Error",
-        statusCode: 500,
-        message: `Error Updating User: ${err.message}`,
-      });
-    }
-  },
 };
 
 module.exports = userController;
