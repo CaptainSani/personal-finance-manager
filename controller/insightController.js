620c6ba8f7c010492fca3ce023afa5c82f3f7ebb
omotomiwa26 <omotomiwa26@gmail.com>
2025-01-06 16:04:35 +0100

    REFACTOR: remove generateMonthlyBreakdown method from insightsController

--- controller/insightController.js
+++ controller/insightController.js
@@ -21,26 +21,6 @@ const insightsController = {
       });
     }
   },
-
-  async generateMonthlyBreakdown(req, res) {
-    try {
-      const { id: userId } = req.user;
-      const breakdown = await Insights.generateMonthlyBreakdown(userId);
-
-      res.status(200).json({
-        status: "Success",
-        statusCode: 200,
-        data: breakdown,
-      });
-    } catch (error) {
-      console.error("Error Generating Monthly Insight Breakdown:", error);
-      res.status(500).json({
-        status: "Internal Server Error",
-        statusCode: 500,
-        message: `Failed To Generate Monthly Insight Breakdown: ${error.message}`,
-      });
-    }
-  },
 };
 
 module.exports = insightsController;
