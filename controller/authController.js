ee561ed6d901c3a411b67efa2bb36ad989d2d81b
omotomiwa26 <omotomiwa26@gmail.com>
2025-01-06 10:08:29 +0100

    REFACTOR: improve formatting and error handling in authController for better readability and maintainability

--- controller/authController.js
+++ controller/authController.js
@@ -1,76 +1,80 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/users');
+const jwt = require("jsonwebtoken");
+const User = require("../models/users");
 
 const authController = {
-    async register(req, res) {
-      try {
-        const { username, email, password } = req.body;
-  
-        if (!username || !email || !password) {
-          return res.status(400).json({ 
-            status: "Bad Request",
-            statusCode: 400,
-            error: 'Please Input All Required Fields' });
-        }
-  
-        await User.createUser(username, email, password);
+  async register(req, res) {
+    try {
+      const { username, email, password } = req.body;
 
-        res.status(200).json({
-          status: "Success OK",
-          statusCode: 200,
-          message: `User ${username} Registered Succesfully` });
-
-        } catch (error) {
-          console.error("Error in registration:", error.message);
-    
-          if (
-            error.message.includes("Username Already Exists") ||
-            error.message.includes("Email Already Exists")
-          ) {
-            return res.status(409).json({
-              status: "Conflict",
-              statusCode: 409,
-              error: error.message,
-            });
-          }
-
-        res.status(500).json({ 
-          status: "Internal Server Error",
-          statusCode: 500,
-          error: 'Registration Failed, Please Try Again Later' });
+      if (!username || !email || !password) {
+        return res.status(400).json({
+          status: "Bad Request",
+          statusCode: 400,
+          error: "Please Input All Required Fields",
+        });
       }
-    },
 
-    async login(req, res) {
-        try {
-          const { email, password } = req.body;
-    
-          if (!email || !password) {
-            return res.status(400).json({ 
-              status: "Bad Request",
-              statusCode: 400,
-              error: 'Please provide all required fields' });
-          }
-    
-          const user = await User.loginUser(email, password);
-          const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
-            expiresIn: process.env.JWT_SECRET_EXPIRY,
-          });
+      await User.createUser(username, email, password);
 
-          res.json({ 
-            status: "Success OK",
-            statusCode: 200,
-            message: `User Logged-In Successfully` , token });
+      res.status(200).json({
+        status: "Success OK",
+        statusCode: 200,
+        message: `User ${username} Registered Succesfully`,
+      });
+    } catch (error) {
+      console.error("Error in registration:", error.message);
 
-        } catch (error) {
-          console.error(error);
-          res.status(401).json({
-            status: "Unathourized",
-            statusCode: 401,
-            error: 'Invalid Email Or Password' });
-        }
-      },
+      if (
+        error.message.includes("Username Already Exists") ||
+        error.message.includes("Email Already Exists")
+      ) {
+        return res.status(409).json({
+          status: "Conflict",
+          statusCode: 409,
+          error: error.message,
+        });
+      }
 
+      res.status(500).json({
+        status: "Internal Server Error",
+        statusCode: 500,
+        error: "Registration Failed, Please Try Again Later",
+      });
+    }
+  },
+
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
+
+      if (!email || !password) {
+        return res.status(400).json({
+          status: "Bad Request",
+          statusCode: 400,
+          error: "Please provide all required fields",
+        });
+      }
+
+      const user = await User.loginUser(email, password);
+      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_SECRET_EXPIRY,
+      });
+
+      res.json({
+        status: "Success OK",
+        statusCode: 200,
+        message: `User Logged-In Successfully`,
+        token,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(401).json({
+        status: "Unathourized",
+        statusCode: 401,
+        error: "Invalid Email Or Password",
+      });
+    }
+  },
 };
 
 module.exports = authController;
